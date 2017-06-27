452332e9e311c2b88b33df8aa85bf297eb9bf655
spmiddlebrooks <spmiddlebrooks@gmail.com>
2017-06-27 08:07:54 -0400

    v1.3.3
    Updated Get-CredentialObject to check for proper username format and continue to prompt user until format is correct
    Updated Connect-O365Skype to check for Hybrid topology and attempt to automatically get TenantName, prompt user if TenantName cannot be determined

--- Connect-O365.psm1
+++ Connect-O365.psm1
@@ -4,8 +4,8 @@
 .PARAMETER
 .EXAMPLE
 .NOTES
-	Version: 1.3.2
-	Updated: 6/5/2017
+	Version: 1.3.3
+	Updated: 6/27/2017
 	Author : Scott Middlebrooks
 .INPUTS
 .OUTPUTS
@@ -44,6 +44,25 @@ function New-SecureStringFile {
 	( (Get-Host).UI.PromptForCredential('Office 365 Credentials','Please enter your Office 365 Admin Password','No Username Required','') ).Password | ConvertFrom-SecureString | Out-File $FilePath
 }
 function Get-CredentialObject {
+	<#
+	.SYNOPSIS
+		Create a credential object for Office 365 authentication
+	.DESCRIPTION
+		A more robust function of Get-Credential with format and error checking
+	.PARAMETER Username
+		Office 365 Administrator username, must take the form of username@domain.com
+	.PARAMETER Password
+		Office 365 Administrator password, can be clear text or the path to a Secure String file
+	.EXAMPLE
+		Get-CredentialObject -Username admin@domain.com -Password MyAdminPassword
+	.EXAMPLE
+		Get-CredentialObject -Username admin@domain.com -Password .\Password.txt
+	.NOTES
+		Version: 1.1
+		Updated: 6/27/2017
+		Author : Scott Middlebrooks
+	.LINK
+	#>
 	[cmdletbinding()]
 	param(
 		[Parameter(Mandatory=$False,Position=0)]
@@ -52,7 +71,9 @@ function Get-CredentialObject {
 			[string] $Password
 	)
 
-	$UsernameRegexMatch = $Username -match "^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$"
+	$UsernameRegexString = "^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$"
+
+	$UsernameRegexMatch = $Username -match $UsernameRegexString
 
 	if ( ($UsernameRegexMatch -AND $Password) -AND (Test-Path $Password) ) {
 		$sPassword = Get-Content $Password | ConvertTo-SecureString
@@ -67,13 +88,23 @@ function Get-CredentialObject {
 			#PromptForCredential(Title,Message,Username,Domain)
 			$CredObj = (Get-Host).UI.PromptForCredential('Office 365 Credentials','Please enter your Office 365 Admin Credentials',$Username,'')
 		}
-		elseif ($Username -AND ! $UsernameRegexMatch) {
-			$wshell = New-Object -ComObject Wscript.Shell
-			$null = $wshell.Popup("Username not a valid userPrincipalName.`nUsername should be of the form username@domain.com.`nPlease re-enter your credentials.",0,"Username Format Invalid",0x30)
-			$CredObj = (Get-Host).UI.PromptForCredential('Office 365 Credentials','Please re-enter your Office 365 Admin Credentials','','')
-		}
+		elseif ($Username -AND -Not $UsernameRegexMatch) {
+			while ($Username -notmatch $UsernameRegexString) {
+				$wshell = New-Object -ComObject Wscript.Shell
+				$null = $wshell.Popup("Username not a valid userPrincipalName.`nUsername should be of the form username@domain.com.`nPlease re-enter your credentials.",0,"Username Format Invalid",0x30)
+				$CredObj = (Get-Host).UI.PromptForCredential('Office 365 Credentials','Please re-enter your Office 365 Admin Credentials','','')
+				$Username = $CredObj.Username
+			}		}
 		else {
 			$CredObj = (Get-Host).UI.PromptForCredential('Office 365 Credentials','Please enter your Office 365 Admin Credentials','','')
+			$Username = $CredObj.Username
+			Write-Debug "Username = $Username"
+			while ($Username -notmatch $UsernameRegexString) {
+				$wshell = New-Object -ComObject Wscript.Shell
+				$null = $wshell.Popup("Username not a valid userPrincipalName.`nUsername should be of the form username@domain.com.`nPlease re-enter your credentials.",0,"Username Format Invalid",0x30)
+				$CredObj = (Get-Host).UI.PromptForCredential('Office 365 Credentials','Please re-enter your Office 365 Admin Credentials','','')
+				$Username = $CredObj.Username
+			}
 		}
 	}
 
@@ -141,19 +172,34 @@ function Connect-O365Skype {
 		[Parameter(Mandatory=$false,Position=3)]
 			[string] $TenantName
 	)
-
+	$ExchangeOnlineHostedVoicemailDestination = 'exap.um.outlook.com'
+	$SkypeOnlineProxyFqdn = 'sipfed.online.lync.com'
+	$SkypeOnlineAutodiscoverUrl = 'https://webdir.online.lync.com/Autodiscover/AutodiscoverService.svc/root'
+	$MicrosoftO365Domain = 'onmicrosoft.com'
+	$TenantNameRegexString = "(\w+)\.(" + $MicrosoftO365Domain + ")?$"
+	
+	
 	if (-Not ($CredentialObject -AND $CredentialObject.GetType().Name -eq 'PSCredential') ) {
 		$CredentialObject = Get-CredentialObject -Username $Username -Password $Password
 	}
 
 	Test-Prerequisites -ServiceName 'O365Skype'
 
-	if ($TenantName) {
-		$OverrideAdminDomain = $TenantName + '.onmicrosoft.com'
+	$HybridEnabled = Get-CsHostingProvider | Where-Object {$_.Enabled -and $_.EnabledSharedAddressSpace -and $_.HostsOCSUsers -and $_.ProxyFqdn -eq $SkypeOnlineProxyFqdn -and $_.AutodiscoverUrl -eq $SkypeOnlineAutodiscoverUrl}
+	$HostedVoicemail = Get-CsHostedVoicemailPolicy | Where-Object {$_.Destination -eq $ExchangeOnlineHostedVoicemailDestination}
+
+	if ($HybridEnabled -and $HostedVoicemail.Organization -match $TenantNameRegexString -and -Not $TenantName) {
+		$TenantName = $HostedVoicemail.Organization
+		$Matches.Clear()
+	}
+	elseif ($HybridEnabled -and -Not $TenantName) {
+		$TenantName = Read-Host -Prompt 'Skype for Business Hybrid Topology Detected, please input your Office 365 Tenant Name'
 	}
 
 	try {
-		if ($OverrideAdminDomain) {
+		if ($TenantName) {
+			$null = $TenantName -match $TenantNameRegexString
+			$OverrideAdminDomain = $Matches[1] + '.' + $MicrosoftO365Domain
 			$session = new-csonlinesession -credential $CredentialObject -OverrideAdminDomain $OverrideAdminDomain
 		}
 		else {
@@ -181,14 +227,14 @@ function Test-Prerequisites {
 		'O365Admin' {  
 			$Program = $InstalledPrograms | ? {$_.DisplayName -match "Microsoft Online.*Sign-In Assistant"}
 
-			if ($Program.VersionMajor -lt 7 -OR !$Program) {
+			if ($Program.VersionMajor -lt 7 -OR -Not $Program) {
 				throw "Microsoft Online Service Sign-In Assistant is not instaled or outdated - major version should be 7 or greater."
 			}
 		}
 		'O365Skype' { 
 			$Program = $InstalledPrograms | ? {$_.DisplayName -match "Skype for Business Online.*PowerShell"}
 
-			if ($Program.VersionMajor -lt 6 -OR !$Program) {
+			if ($Program.VersionMajor -lt 6 -OR -Not $Program) {
 				throw "Microsoft Skype for Business Online PowerSell Module not installed or outdated - major version should be 6 or greater."
 			}
 		}
